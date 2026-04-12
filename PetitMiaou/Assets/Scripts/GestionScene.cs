459e72c0f00b9851c82ab04b5ac9991100e0d708
Cazalie <96954594+Cazalie@users.noreply.github.com>
2026-04-12 01:44:52 -0400

    Finissions des images

--- PetitMiaou/Assets/Scripts/GestionScene.cs
+++ PetitMiaou/Assets/Scripts/GestionScene.cs
@@ -3,26 +3,40 @@ using UnityEngine.SceneManagement;
 
 public class GestionScene : MonoBehaviour
 {
-    public void ChargerScene()
+    public void ChargerNiveau1()
     {
-        SceneManager.LoadScene("Niveau1");
+        SceneManager.LoadScene(1);
         Debug.Log("Niveau 1 chargé");
-        //SceneManager.LoadScene(1);
+        
     }
+
+    public void ChargerNiveau2()
+    {
+        SceneManager.LoadScene(2);
+        Debug.Log("Niveau 2 chargé");
+    }
+
+    public void ChargerNiveau3()
+    {
+        SceneManager.LoadScene(3);
+        Debug.Log("Niveau 3 chargé");
+    }
+
     public void RetourMenu()
     {
         SceneManager.LoadScene(0);
         Debug.Log("Menu chargé");
     }
+
     public void RechargerCourant()
     {
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
         Debug.Log("Scène rechargée : " + scene.name);
     }
-    public void QuitterJeu()
+    public void Muet()
     {
-        Application.Quit();
+        Debug.Log("Muet");
     }
     
     
